393809d72cf4cb83d90f5cb9dd5967984121bef9
kmax.eth <92766553+maxkaneco@users.noreply.github.com>
2022-10-02 12:18:32 +0800

    version API for auditor and depot (#7)

--- depot/depot.pb.gw.go
+++ depot/depot.pb.gw.go
@@ -192,6 +192,24 @@ func local_request_Depot_DownloadChunk_0(ctx context.Context, marshaler runtime.
 
 }
 
+func request_Depot_GetVersion_0(ctx context.Context, marshaler runtime.Marshaler, client DepotClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq emptypb.Empty
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.GetVersion(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Depot_GetVersion_0(ctx context.Context, marshaler runtime.Marshaler, server DepotServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq emptypb.Empty
+	var metadata runtime.ServerMetadata
+
+	msg, err := server.GetVersion(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterDepotHandlerServer registers the http handlers for service Depot to "mux".
 // UnaryRPC     :call DepotServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -320,6 +338,31 @@ func RegisterDepotHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 		}
 
 		forward_Depot_DownloadChunk_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_Depot_GetVersion_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/photon.depot.Depot/GetVersion", runtime.WithHTTPPathPattern("/photon/depot/version"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Depot_GetVersion_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Depot_GetVersion_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -474,6 +517,28 @@ func RegisterDepotHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("GET", pattern_Depot_GetVersion_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/photon.depot.Depot/GetVersion", runtime.WithHTTPPathPattern("/photon/depot/version"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Depot_GetVersion_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Depot_GetVersion_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -487,6 +552,8 @@ var (
 	pattern_Depot_UploadChunk_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"photon", "depot", "upload_chunk"}, ""))
 
 	pattern_Depot_DownloadChunk_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"photon", "depot", "download_chunk"}, ""))
+
+	pattern_Depot_GetVersion_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"photon", "depot", "version"}, ""))
 )
 
 var (
@@ -499,4 +566,6 @@ var (
 	forward_Depot_UploadChunk_0 = runtime.ForwardResponseMessage
 
 	forward_Depot_DownloadChunk_0 = runtime.ForwardResponseMessage
+
+	forward_Depot_GetVersion_0 = runtime.ForwardResponseMessage
 )
